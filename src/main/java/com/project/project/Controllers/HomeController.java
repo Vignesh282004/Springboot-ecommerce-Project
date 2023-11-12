59690761d113a2c354fc69c4a15050893d9fb781
Vignesh <120371618+Vignesh282004@users.noreply.github.com>
2023-11-12 11:28:14 +0530

    Update HomeController.java
    Added a Basic Contact form

--- src/main/java/com/project/project/Controllers/HomeController.java
+++ src/main/java/com/project/project/Controllers/HomeController.java
@@ -6,13 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.project.dto.CategoryDto;
 import com.project.project.dto.ProductDto;
 import com.project.project.model.Category;
+import com.project.project.model.Contact;
 import com.project.project.model.Product;
 import com.project.project.repository.CategoryRepo;
 import com.project.project.service.serviceimpl.CAtegoryservimpl;
+import com.project.project.service.serviceimpl.MailServImpl;
 import com.project.project.service.serviceimpl.ProductServiceImpl;
 
 
@@ -26,6 +31,8 @@ public class HomeController {
 	private ProductServiceImpl productServiceImpl;
 	@Autowired
 	private CAtegoryservimpl cAtegoryservimpl;
+	@Autowired
+	private MailServImpl mailServImpl;
 	@GetMapping("/index")
 	public String index(Model model)
 	{
@@ -48,11 +55,29 @@ public class HomeController {
 		model.addAttribute("listViewProducts",listViewProducts);
 		return "shop";
 	}
+@GetMapping("/contact")
+	public String contact(Model model)
+	{
+		model.addAttribute("contact",new Contact());
+		return "contact";
+	}
+
+@PostMapping("/contactform")
+public String sendContact(@ModelAttribute("contact")Contact contact,Model model,RedirectAttributes attributes)
+{
 	
-	 @GetMapping("/contact")
-	    public String contact(Model model) {
-	        return "contact";
-	    }
-	 
-	 
+	try {
+		mailServImpl.SendEmail(contact.getName(),contact.getEmail(),contact.getSubject(),contact.getMessage());
+		attributes.addFlashAttribute("message","Email Sent Succesfully");
+		System.out.println("Email sent....");
+	} catch (Exception e) {
+		System.out.println(e);
+		System.out.println("Email not sent");
+		attributes.addFlashAttribute("message","Email Not Sent");
+	}
+	return "contact";
 }
+}
+	 
+	 
+
