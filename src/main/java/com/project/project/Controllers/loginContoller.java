c9b025e98ad32b03266434cbb8f6281364b1ec55
Vignesh <120371618+Vignesh282004@users.noreply.github.com>
2023-11-12 11:26:51 +0530

    Update loginContoller.java
    Added forgotpassword feature

--- src/main/java/com/project/project/Controllers/loginContoller.java
+++ src/main/java/com/project/project/Controllers/loginContoller.java
@@ -1,7 +1,10 @@
 package com.project.project.Controllers;
 import java.security.Principal;
+import java.util.Random;
 
+import org.hibernate.bytecode.internal.bytebuddy.PrivateAccessorException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.event.PublicInvocationEvent;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,14 +15,19 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.project.dto.AdminDto;
 import com.project.project.model.Admin;
+import com.project.project.model.Customer;
+import com.project.project.repository.CustomerRepo;
 import com.project.project.service.AdminService;
+import com.project.project.service.serviceimpl.MailServImpl;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import scala.annotation.elidable;
 
 @Controller
 public class loginContoller {
@@ -30,6 +38,13 @@ public class loginContoller {
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
+	
+	@Autowired
+	private MailServImpl mailServImpl;
+	
+	@Autowired
+	private CustomerRepo customerRepo;
+
 	@GetMapping("/login")
 	public String login()
 	{
@@ -105,6 +120,95 @@ public class loginContoller {
 		return "Cforgot-password";
 	}
 
+	@PostMapping("/send-otp")
+	public String sendotp(@RequestParam("email")String email ,Model model,RedirectAttributes attributes,HttpSession session)
+	{
+		System.out.println(email);
+		
+		Random random = new Random(); 
+		int otp = random.nextInt(999999);
+		System.out.println(otp);
+		String x_email = email;
+		String subject = "This is Otp From Spring-boot App";
+		String message = "Your Otp of Spirng boot Application-Login is : "+otp+"";
+	boolean flag = this.mailServImpl.SendEmail(x_email,subject,message);
+	if(flag)
+	{
+		session.setAttribute("oldotp", otp);
+		session.setAttribute("email", email);
+		model.addAttribute("success","Otp Was Sent Suucessfully ! Check your Mail");
+		return "otp-check";
+	}
+	else 
+	{
+		attributes.addFlashAttribute("failed","Otp Was Not Sent ___ Server Down <===========>");
+		session.setAttribute("message", "OOPS ?* Server Runtime Error ------- Otp Not Sent");
+		return "Cforgot-password";
+	}
+	}
+	
+	@PostMapping("/otp-check")
+	public String verify_otp(@RequestParam("otp") int otp,HttpSession session,Model model)
+	{
+		int oldotp  = (int)session.getAttribute("oldotp");
+		String email = (String)session.getAttribute("email");
+		if(otp == oldotp)
+		{
+			Customer customer = this.customerRepo.findByUsername(email);
+			if(customer==null)
+			{
+				model.addAttribute("failed","No User Existed..Please Go back and Register First *");
+				return "otp-check";
+			}
+			else {
+				
+				return "change_password";
+			}
+		}
+		else 
+		{
+			model.addAttribute("failed","You Have Entered Wrong Otp !! Please Enter Latest Otp");
+			return "otp-check";
+		}
+		
+	}
 	
 	
+	@PostMapping("/change-pass")
+	public String changepass(@RequestParam("password")String password,Model model,HttpSession session)
+	{
+		try {
+			String email =(String)session.getAttribute("email");
+			Customer customer = this.customerRepo.findByUsername(email);
+			customer.setPassword(this.passwordEncoder.encode(password));
+			this.customerRepo.save(customer);
+			model.addAttribute("success","Password Changed Successfully.");
+			return "change_password";
+		} catch (Exception e) {
+			// TODO: handle exception
+			model.addAttribute("failed","Password Didn't Change");
+			System.out.println(e);
+		}
+		return "change_password";
+	
+	}
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
